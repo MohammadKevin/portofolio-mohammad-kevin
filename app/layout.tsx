b5eddbd03e1087a45b519d4ac19a71d95056ec82
MohammadKevin <kvn4.200581@gmail.com>
2025-12-29 10:47:59 +0700

    Update title and content

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export const metadata = {
-  title: "Rudi | Developer Portfolio",
+  title: "Mohammad Kevin | Developer Portfolio",
   description: "Backend & Fullstack Developer",
 };
 
